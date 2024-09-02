f7c93e1ea858c4b25b0126d7d70577aaac9a489f
mjpavithra <mjpavithra6520@gmail.com>
2024-09-02 10:03:28 +0530

    commit befor pull

--- backend/index.ts
+++ backend/index.ts
@@ -4,14 +4,20 @@ import sampleRoute from './routes/sampleRoute';
 const app: Application = express();
 const port = process.env.PORT || 5000;
 
+const cors = require('cors');
+
 // Middleware
 app.use(express.json());
-
+app.use(cors());
 // Routes
 app.use('/api/sample', sampleRoute);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, TypeScript with Node.js!');
+});
+
+app.get('/api', (req: Request, res: Response) => {
+  res.send('Hello, TypeScript with Node.js! This is an API endpoint');
 });
 
 app.listen(port, () => {
