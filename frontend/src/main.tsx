df4c11ea60705ea6710cd4383da3e9c381eb36f5
anjuna0305 <anjunaserasingha@gmail.com>
2024-09-06 03:53:42 +0530

    frontend: strict mode removed

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -25,7 +25,5 @@ const router = createBrowserRouter([
 console.log('Hello, world!')
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <RouterProvider router={router} />
-    </React.StrictMode>,
+    <RouterProvider router={router} />
 )
