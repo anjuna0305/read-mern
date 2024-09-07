a207bf50036f704b680e5fbec5305dc132a42566
anjuna0305 <anjunaserasingha@gmail.com>
2024-09-07 14:36:47 +0530

    backend: search function added

--- backend/src/controllers/stockController.ts
+++ backend/src/controllers/stockController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express'
-import mongoose from 'mongoose'
+import mongoose  from 'mongoose'
 import StockItem from '../models/stockItemModel'
 
 
@@ -33,6 +33,7 @@ export const createStockItem = async (req: Request, res: Response) => {
 
 // GET ALL STOCK ITEMS
 export const getAllStockItems = async (req: Request, res: Response) => {
+    console.log("get all items called")
     try {
         const items = await StockItem.find()
         res.status(200).json(items)
@@ -91,3 +92,38 @@ export const deleteStockItem = async (req: Request, res: Response) => {
         res.status(500).json({ message: 'Failed to delete stock item', error: (err as Error).message })
     }
 }
+
+// SEARCH ITEMS
+export const searchStockItemsByName = async (req: Request, res: Response) => {
+    try {
+        const { name } = req.query
+
+        if (!name || typeof name !== 'string') {
+            return res.status(400).json({ message: 'Please provide a valid name to search for.' })
+        }
+
+        const items = await searchStockItems(name)
+
+        if (items.length === 0) {
+            return res.status(404).json({ message: `No stock items found matching "${name}".` })
+        }
+
+        res.status(200).json(items);
+    } catch (err: unknown) {
+        res.status(500).json({ message: 'Failed to search stock items', error: (err as Error).message })
+    }
+};
+
+
+// uitility functions
+export const searchStockItems = async (name: string) => {
+    if (!name || typeof name !== 'string') {
+        throw new Error('Invalid name provided for search');
+    }
+
+    const items = await StockItem.find({
+        itemName: { $regex: new RegExp(name, 'i') } // i for case insensitive
+    });
+
+    return items;
+};
