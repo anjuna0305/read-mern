a8fd96c4f01b2b89d1a192230a4c8d9037f2e1ab
mjpavithra <mjpavithra6520@gmail.com>
2024-09-09 14:52:04 +0530

    main.ts edited

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,70 +1,68 @@
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import HomePage from './pages/HomePage'
-import Orders from './pages/Orders'
-import TestPage from './pages/TestPage'
-import AddUserPage from './pages/AddUser'
-import LoginPage from './pages/TestPage' // Import LoginPage
-import { AuthProvider } from './context/AuthContext' // Import AuthProvider
-import PrivateRoute from './Components/PrivateRoute' // Import PrivateRoute
-import StockPage from './pages/StockPage'
-import StockSearch from './Containers/StockSearch'
-import CreateNewStockItem from './Components/CreateNewStockItem'
-import AllStockItems from './Components/AllStockItems'
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AllStockItems from "./Components/AllStockItems";
+import CreateNewStockItem from "./Components/CreateNewStockItem";
+import PrivateRoute from "./Components/PrivateRoute"; // Import PrivateRoute
+import StockSearch from "./Containers/StockSearch";
+import { AuthProvider } from "./context/AuthContext"; // Import AuthProvider
+import "./index.css";
+import AddUserPage from "./pages/AddUser";
+import HomePage from "./pages/HomePage";
+import Orders from "./pages/Orders";
+import StockPage from "./pages/StockPage";
+import { default as LoginPage, default as TestPage } from "./pages/TestPage";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <HomePage />,
-    },
-    {
-        path: "/orders",
-        element: (
-        <PrivateRoute requiredRole="cashier">
-                <Orders />
-        </PrivateRoute>),
-    },
-    {
-        path: "/test",
-        element: <TestPage />,
-    },
-    {
-        path: "/adduser",
-        element: (
-            <PrivateRoute requiredRole="admin">
-                <AddUserPage />
-            </PrivateRoute>
-        ), 
-    },
-    {
-        path: "/stock",
-        element: <StockPage />,
-        children: [
-            {
-                path: "search",
-                element: <StockSearch />
-            },
-            {
-                path: "add-new",
-                element: <CreateNewStockItem/>
-            },
-            {
-                path: "all",
-                element: <AllStockItems/>
-            }
-        ]
-    }
-])
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/orders",
+    element: (
+      <PrivateRoute requiredRole="cashier">
+        <Orders />
+      </PrivateRoute>
+    ),
+  },
+  {
+    path: "/test",
+    element: <TestPage />,
+  },
+  {
+    path: "/adduser",
+    element: (
+      <PrivateRoute requiredRole="admin">
+        <AddUserPage />
+      </PrivateRoute>
+    ),
+  },
+  {
+    path: "/stock",
+    element: <StockPage />,
+    children: [
+      {
+        path: "search",
+        element: <StockSearch />,
+      },
+      {
+        path: "add-new",
+        element: <CreateNewStockItem />,
+      },
+      {
+        path: "all",
+        element: <AllStockItems />,
+      },
+    ],
+  },
+  {
+    path: "/login", // Add login route
+    element: <LoginPage />,
+  },
+]);
 
-    {
-        path: "/login", // Add login route
-        element: <LoginPage />,
-    },
-])
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-        <AuthProvider>
-            <RouterProvider router={router} />
-        </AuthProvider>
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <AuthProvider>
+    <RouterProvider router={router} />
+  </AuthProvider>
+);
